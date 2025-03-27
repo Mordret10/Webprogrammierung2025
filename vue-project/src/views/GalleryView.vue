c5ab0470acfe3f3a8c4be353ab4278ec0847af5a
Mordret10 <jujo.flaccus@gmail.com>
2025-03-27 12:25:13 +0100

    Added functionality to Gallery, improved style

--- vue-project/src/views/GalleryView.vue
+++ vue-project/src/views/GalleryView.vue
@@ -1,15 +1,61 @@
-<script setup>
-
+<script>
+  export default {
+    data() {
+      return {
+        images: ["/GalleryPics/Gallery_Blaske.jpg", "/GalleryPics/Gallery_Eschwege.jpg", "/GalleryPics/Gallery_GCK.jpg", "/GalleryPics/Gallery_Goessnitz.jpg", "/GalleryPics/Gallery_Subway.jpg"],
+        index: 0
+      }
+    },
+    methods: {
+      increase(){
+        if(this.index === 4)
+        {
+          this.index = 0;
+        }
+        else {
+          this.index += 1;
+        }
+      },
+      decrease(){
+        if(this.index === 0)
+        {
+          this.index = 4;
+        }
+        else {
+          this.index -= 1;
+        }
+      }
+    }
+  }
 </script>
 
 <template>
-  <div class="gallery"
-  ></div>
+  <h1>Gallery</h1>
+  <div class="buttons">
+    <button @click="decrease">Previous</button>
+    <button @click="increase">Next</button>
+  </div>
   <div>
-    <img alt="Katja Image" src="/KSV_Portrait.jpg" width="125" height="125" />
+    <img alt="Image" :src="images[index]" width="1000" height="1000" />
   </div>
 </template>
 
-<style scoped>
+<style>
 
+
+img {
+  object-fit: contain;
+}
+
+div.buttons {
+  padding-top: 1rem;
+  display: flex;
+  justify-content: center;
+  place-content: center;
+  flex-wrap: wrap;
+}
+
+button {
+  font-size: 1rem;
+}
 </style>
--- vue-project/src/views/OverviewBookingsView.vue
+++ vue-project/src/views/OverviewBookingsView.vue
@@ -4,6 +4,8 @@ import {RouterLink} from "vue-router";
 </script>
 
 <template>
+  <h1>Booking Overview</h1>
+  <p>Select your type of booking:</p>
   <nav>
     <RouterLink to="/dryhire">Dry hire Equipment</RouterLink>
     <RouterLink to="/musician">Hire as musician</RouterLink>
@@ -11,6 +13,6 @@ import {RouterLink} from "vue-router";
   </nav>
 </template>
 
-<style scoped>
+<style>
 
 </style>
